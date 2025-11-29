fabe1092b15f854691192876a1132fb0123d89c8
Christian Lenz <Christian.Lenz@gmx.net>
2025-11-29 17:14:52 +0100

    Split RestClient action in 2 seperate ones for opening UI and editor. Make some code parts reusable

--- src/main/java/com/javierllorente/netbeans/rest/client/http/editor/sidebar/ActionPopup.java
+++ src/main/java/com/javierllorente/netbeans/rest/client/http/editor/sidebar/ActionPopup.java
@@ -22,15 +22,11 @@ package com.javierllorente.netbeans.rest.client.http.editor.sidebar;
 
 import com.javierllorente.netbeans.rest.client.http.editor.sidebar.request.IRequestProcessor;
 import com.javierllorente.netbeans.rest.client.http.editor.sidebar.request.Request;
-import com.javierllorente.netbeans.rest.client.http.editor.syntax.antlr.HTTPParser;
-import com.javierllorente.netbeans.rest.client.ui.RestClientTopComponent;
 import java.awt.event.ActionEvent;
 import javax.swing.Icon;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
-import javax.swing.SwingUtilities;
 import org.openide.util.ImageUtilities;
-import org.openide.windows.WindowManager;
 
 /**
  * Popup menu for running and debugging tasks.
@@ -45,7 +41,7 @@ public class ActionPopup {
 
     public JPopupMenu createPopupMenu(Request request) {
         Icon runIcon = ImageUtilities.loadImageIcon("org/netbeans/modules/project/ui/resources/runProject.png", false);
-        Icon uiIcon = ImageUtilities.loadImageIcon("com/javierllorente/netbeans/rest/client/http/editor/restservice.png", false);
+        Icon uiIcon = ImageUtilities.loadImageIcon("com/javierllorente/netbeans/rest/client/restservice.png", false);
 
         JPopupMenu popupMenu = new JPopupMenu();
 
@@ -60,54 +56,10 @@ public class ActionPopup {
         // Open in UI action
         JMenuItem openInUIItem = new JMenuItem("Open in UI", uiIcon);
         openInUIItem.addActionListener((ActionEvent e) -> {
-            openInUI(request.getrequestContext());
+            requestProcessor.openRequestInUi(request.getrequestContext());
         });
         popupMenu.add(openInUIItem);
 
         return popupMenu;
-    }
-
-        return popupMenu;
-    }
-
-    /**
-     * Opens the RestClientTopComponent and populates it with data from the HTTP request.
-     */
-    private void openInUI(HTTPParser.RequestContext requestContext) {
-        if (requestContext == null) {
-            return;
-        }
-
-        SwingUtilities.invokeLater(() -> {
-            RestClientTopComponent tc = (RestClientTopComponent) WindowManager.getDefault().findTopComponent("RestClientTopComponent");
-            if (tc != null) {
-                tc.open();
-                tc.requestActive();
-
-                // Extract request data
-                HTTPParser.RequestLineContext requestLine = requestContext.requestLine();
-                if (requestLine != null) {
-                    // Extract method
-                    String method = "GET"; // Default
-                    if (requestLine.METHOD() != null) {
-                        method = requestLine.METHOD().getText();
-                    }
-
-                    // Extract URL/target
-                    String url = "";
-                    HTTPParser.RequestTargetContext target = requestLine.requestTarget();
-                    if (target != null) {
-                        url = target.getText();
-                    }
-
-                    // TODO: Extract headers from requestContext.requestHeaders()
-                    // TODO: Extract body from requestContext.requestBodySection()
-
-                    // Populate the UI
-                    tc.setRequestMethod(method);
-                    tc.setUrl(url);
-                }
-            }
-        });
     }
 }
